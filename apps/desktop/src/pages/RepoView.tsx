b05b6cfaed1b04e70b016af52a9aed5a866c43e9
thienlvc (VSF-TMDTQC) <v.thienlvc@vinsmartfuture.tech>
2026-06-03 12:02:44 +0700

    chore(ui): remove unused TitleBar import

--- apps/desktop/src/pages/RepoView.tsx
+++ apps/desktop/src/pages/RepoView.tsx
@@ -1,10 +1,8 @@
 import MainLayout from "@/layouts/MainLayout";
-import TitleBar from "@/components/layout/TitleBar";
 
 export default function RepoView() {
   return (
     <div className="h-screen w-screen flex flex-col overflow-hidden bg-surface-0">
-      {/* <TitleBar /> */}
       <div className="flex-1 min-h-0 overflow-hidden">
         <MainLayout />
       </div>
